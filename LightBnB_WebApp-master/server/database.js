18e73bab6629ffd0219c20528435e8b903efce32
Zio <zi.ao.tan@outlook.com>
2021-05-27 22:32:41 +0000

    Add minimum rating in search query

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -172,12 +172,24 @@ const getAllProperties = (options, limit = 10) => {
     }
 
     // 4
+
+    queryString += `GROUP BY properties.id `
+
+    if (options.minimum_rating) {
+      queryParams.push(parseFloat(options.minimum_rating));
+      queryString += `HAVING avg(property_reviews.rating) >= $${queryParams.length} `;
+    }
+
     queryParams.push(limit);
     queryString += `
-    GROUP BY properties.id
     ORDER BY cost_per_night
     LIMIT $${queryParams.length};
     `;
+   
+   
+
+
+
   
     // 5
     console.log(queryString, queryParams);
